25b630f4a75ec5b9216f4800a3c4ab8126bee55e
github username <github email address>
2020-04-22 12:15:34 -0500

    signup working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,32 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+
+  # @user = User.create(
+  #   name: params[:name],
+  #   email: params[:email],
+  #   password_digest: params[:password_digest]
+  # )
 
   def create
-  user = User.create(
-      name: params[:name],
-      email: params[:email],
-      password_digest: params[:password_digest]
-  )
-    if user.valid?
-      render json: user, status: :created
+   @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   def profile
-  
+    render json: { user: current_user }, status: :accepted
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password_digest, :email)
+  end
+
      
 end
